2619573f4b68677a3373cc85875392989040b786
Ricardo Werner <wernerdive@gmail.com>
2024-12-07 15:04:41 -0300

    feat: add funcao para acao nos cards da home

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,24 @@
-console.log ('Hello, World!');
+/*card home animation*/
+function handleMouseEnter() {
+  this.classList.add("ms-card--hover");
+  document.body.id = `${this.id}-hover`;
+}
+
+function handleMouseLeave() {
+  this.classList.remove("ms-card--hover");
+  document.body.id = "";
+}
+
+function addEventListenersToCard() {
+  const card = document.getElementsByClassName("ms-card");
+
+  for (let i = 0; i < card.length; i++) {
+    const cardElement = card[i]; 
+    cardElement.addEventListener("mouseenter", handleMouseEnter);
+    cardElement.addEventListener("mouseleave", handleMouseLeave);
+}
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCard, false);
+
+
